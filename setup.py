a31a28f1273aab71a303e3fc50e750264e30eeba
xiaohan-zhang <xiaohan.zhang@salesforce.com>
2020-07-14 14:26:04 -0700

    Remove version constraints in setup.py except tensorflow <2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 import sys
 
 assert sys.version_info.major == 3 and sys.version_info.minor >= 6, \
-    "The Spinning Up repo is designed to work with Python 3.6 and greater." \
+    "The SDERL repo is designed to work with Python 3.6 and greater." \
     + "Please install it before proceeding."
 
 with open(join("sderl", "version.py")) as version_file:
@@ -14,22 +14,22 @@ setup(
     py_modules=['sderl'],
     version=__version__,#'0.1',
     install_requires=[
-        'cloudpickle==1.2.1',
-        'gym[atari,box2d,classic_control]~=0.15.3',
+        'cloudpickle',
+        'gym[atari,box2d,classic_control]',
         'ipython',
         'joblib',
-        'matplotlib==3.1.1',
+        'matplotlib',
         'mpi4py',
         'numpy',
         'pandas',
         'pytest',
         'psutil',
         'scipy',
-        'seaborn==0.8.1',
+        'seaborn',
         'tensorflow>=1.8.0,<2.0',
-        'torch==1.3.1',
+        'torch',
         'tqdm'
     ],
-    description="Teaching tools for introducing people to deep RL.",
-    author="Joshua Achiam",
+    description="research on stochastic differential equations and deep RL.",
+    author="Xiaohan Zhang",
 )
