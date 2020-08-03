3f0092603de5ecdf7c186683c4235fd19920879c
xiaohan-zhang <xiaohan.zhang@salesforce.com>
2020-08-03 10:48:55 -0700

    add dqn to algos/__init__

--- sderl/__init__.py
+++ sderl/__init__.py
@@ -5,6 +5,7 @@ from sderl.algos.pytorch.ppo.ppo import ppo as ppo_pytorch
 from sderl.algos.pytorch.dppo.dppo import dppo as dppo_pytorch
 from sderl.algos.pytorch.sac.sac import sac as sac_pytorch
 from sderl.algos.pytorch.sac_atari.sac import sac_atari as sac_atari_pytorch
+from sderl.algos.pytorch.dqn.dqn import dqn as dqn_pytorch
 from sderl.algos.pytorch.td3.td3 import td3 as td3_pytorch
 from sderl.algos.pytorch.trpo.trpo import trpo as trpo_pytorch
 from sderl.algos.pytorch.vpg.vpg import vpg as vpg_pytorch
