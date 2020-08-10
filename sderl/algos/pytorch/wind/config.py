ef16cfe19c5b2a574f8f7f17fac8a5f50356901d
xiaohan-zhang <xiaohan.zhang@salesforce.com>
2020-08-10 10:28:46 -0700

    Add MC simulator for HJB to wind

--- sderl/algos/pytorch/wind/config.py
+++ sderl/algos/pytorch/wind/config.py
@@ -27,9 +27,10 @@ class HJBConfig(Config):
     # Y_0 is about 4.5901.
     dim = 100
     total_time = 1.0
-    num_time_interval = 20
+    num_time_interval = 3
+    train_iters=3000
     lr_boundaries = [400]
-    num_iterations = 2000
+    num_iterations = 200000
     lr_values = list(np.array([1e-2, 1e-2]))
     num_hiddens = [dim, dim+10, dim+10, dim]
     y_init_range = [0, 1]
